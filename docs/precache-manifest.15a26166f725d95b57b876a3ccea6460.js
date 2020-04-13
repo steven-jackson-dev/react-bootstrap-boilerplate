5f7210c9b91a5bb065e777c982fc8531931f43cb
Steven <stevenjackson-dev@gmail.com>
2020-04-13 15:55:54 +0200

    first commit

---
+++ docs/precache-manifest.15a26166f725d95b57b876a3ccea6460.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57ea97b984b3af3591c4d04168686f3",
+    "url": "/react-bootstrap-boilerplate/index.html"
+  },
+  {
+    "revision": "77924c8376ba18dd85d8",
+    "url": "/react-bootstrap-boilerplate/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "802b1ab6130961febbc3",
+    "url": "/react-bootstrap-boilerplate/static/css/main.8ec21848.chunk.css"
+  },
+  {
+    "revision": "77924c8376ba18dd85d8",
+    "url": "/react-bootstrap-boilerplate/static/js/2.d6235983.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-bootstrap-boilerplate/static/js/2.d6235983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "802b1ab6130961febbc3",
+    "url": "/react-bootstrap-boilerplate/static/js/main.234b0898.chunk.js"
+  },
+  {
+    "revision": "458a4a92d5ded07e9a70",
+    "url": "/react-bootstrap-boilerplate/static/js/runtime-main.10cf3a89.js"
+  }
+]);
